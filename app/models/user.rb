773ba4915296ea1c8781cd8d39baad703fbae0ae
Daniel Meschiany <daniel@minute.ly>
2016-03-28 00:08:38 +0300

    db connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :f_name, :l_name, :college_id, :password, :last_login_timestamp
   has_many :contents
   has_many :videos
-  has_many :courses through :video
+  has_many :courses, through: :video
 end
